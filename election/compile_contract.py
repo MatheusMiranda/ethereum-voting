63135ba083726d50cf5802b36085e1626e671628
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-04-22 21:10:32 -0300

    Udpate Voting contract
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- election/compile_contract.py
+++ election/compile_contract.py
@@ -8,21 +8,49 @@ from web3.contract import ConciseContract
 #compiled_sol = compile_files('contracts/Voting.sol') # Compiled source code
 
 contract_source_code = '''
-pragma solidity ^0.4.21;
+pragma solidity 0.4.25;
 
-contract Greeter {
-    string public greeting;
-
-    function Greeter() public {
-        greeting = 'Hello';
+contract Voting {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+        bool definedCandidate; 
     }
 
-    function setGreeting(string _greeting) public {
-        greeting = _greeting;
+    uint public numCandidates; 
+    mapping (string => Candidate) candidates;
+    mapping (address => bool) voters;
+
+    event votedEvent (
+        string _candidateName
+    );
+
+    function Voting () public {
+        addCandidate("Candidate_1");
+        addCandidate("Candidate_2");
+    }  
+
+    function addCandidate(string _name) private {
+        numCandidates ++;
+        candidates[_name] = Candidate(numCandidates,_name,0,true);
     }
 
-    function greet() view public returns (string) {
-        return greeting;
+    function vote (string _candidateName) public {
+        // require that they haven't voted before
+        // require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(candidates[_candidateName].definedCandidate == true);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateName].voteCount ++;
+
+        // trigger voted event
+        votedEvent(_candidateName);
     }
 }
 '''
