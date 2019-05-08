99728f614d08a798ab44ebaad14fe488195a167e
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-05-08 11:47:30 -0300

    Add service to process accounts addresses
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- election/README.md
+++ election/README.md
@@ -5,3 +5,7 @@ sudo docker-compose run --rm ethereum-compiler make deploy-contract
 To cast a vote for a candidate use the following request:
 
 curl -H "Content-Type: application/json" --request POST -d '{"candidate_name":"Candidate_1"}' http://localhost:5000/blockchain/voting
+
+To generate account run the command:
+
+sudo docker-compose run --rm geth-client /etc/blockchain_config/setup_blockchain.sh
--- election/config/setup_blockchain.sh
+++ election/config/setup_blockchain.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd /etc/blockchain_config/
 
 #Remove old blockchain data
@@ -5,8 +7,9 @@ rm -rf datadir
 mkdir datadir
 
 #Setup accounts
-geth --datadir=./datadir --password ./password_1.txt account new >> accounts.txt
-geth --datadir=./datadir --password ./password_1.txt account new >> accounts.txt
-
-#Init BlockChain
-geth --datadir ./datadir init genesis_block.json
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_1) account new >> accounts.txt
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_2) account new >> accounts.txt
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_3) account new >> accounts.txt
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_4) account new >> accounts.txt
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_5) account new >> accounts.txt
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_6) account new >> accounts.txt
