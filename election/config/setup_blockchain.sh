0d8271a17972fc270b8e826909c5185c7b12fc19
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-05-09 15:53:12 -0300

    Add script to init accounts
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- election/config/setup_blockchain.sh
+++ election/config/setup_blockchain.sh
@@ -7,9 +7,9 @@ rm -rf datadir
 mkdir datadir
 
 #Setup accounts
-geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_1) account new >> accounts.txt
-geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_2) account new >> accounts.txt
-geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_3) account new >> accounts.txt
-geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_4) account new >> accounts.txt
-geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_5) account new >> accounts.txt
-geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_6) account new >> accounts.txt
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_1) account new >> accounts.json
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_2) account new >> accounts.json
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_3) account new >> accounts.json
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_4) account new >> accounts.json
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_5) account new >> accounts.json
+geth --datadir=./datadir --password <(echo $PASSWORD_ACCOUNT_6) account new >> accounts.json
